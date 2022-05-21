ab40f7b81cb7c57603009a3880be08a2d926725a
Jason Quach <thuanqh@gmail.com>
2022-05-21 10:12:38 +0700

    Updates

---
+++ component---src-pages-404-js-2b680be81d9b6e913b6f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklungvang_vn=self.webpackChunklungvang_vn||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294),l=n(410),a=n(262);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2b680be81d9b6e913b6f.js.map
